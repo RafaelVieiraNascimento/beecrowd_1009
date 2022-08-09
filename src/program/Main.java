8c190ad403349db929bbf0d6fa2c879ea571dddb
RafaelVN <contato@RafaelVN.com>
2022-08-09 15:42:22 -0300

    beecrowd_1009

---
+++ src/program/Main.java
@@ -0,0 +1,34 @@
+package program;
+
+/*
+beecrowd | 1009 - Salário com Bônus
+
+Faça um programa que leia o nome de um vendedor, o seu salário fixo
+e o total de vendas efetuadas por ele no mês (em dinheiro).
+Sabendo que este vendedor ganha 15% de comissão sobre suas vendas efetuadas,
+informar o total a receber no final do mês, com duas casas decimais.
+
+Entrada
+O arquivo de entrada contém um texto (primeiro nome do vendedor)
+e 2 valores de dupla precisão (double) com duas casas decimais,
+representando o salário fixo do vendedor e montante total das vendas efetuadas por este vendedor, respectivamente.
+
+Saída
+Imprima o total que o funcionário deverá receber, conforme exemplo fornecido.
+ */
+
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String nome = sc.next();
+		double salario = sc.nextDouble();
+		double vendas = sc.nextDouble();
+		double total = salario + vendas * 15/100;
+		
+		System.out.printf("TOTAL = R$ %.2f\n",total );
+		sc.close();
+	}
+}
